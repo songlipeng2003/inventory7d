75e2822b2d029891b997d68aeef5a86923cd40ba
Thinking Song <123wuming_123@163.com>
2013-12-06 20:55:46 +0800

    初始化项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inventory7d::Application.config.secret_key_base = 'dd84515a85026b69ced7c94505a5be5f214a53aa05e951f3b25dc175f2f678c1dc6673a6df570c5b4687fce208b26263b2abfe99c1d5a1d0b4348021dfaea1d1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Inventory7d::Application.config.session_store :cookie_store, key: '_inventory7d_session'
