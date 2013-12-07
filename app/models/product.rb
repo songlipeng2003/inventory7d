d4e7832af652d56be7b8ef2f880b6389f6421842
Thinking Song <123wuming_123@163.com>
2013-12-07 21:44:54 +0800

    添加产品的验证

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+  validates :sn, :format => { with: /\d+/, message: "必须全部为数字" }
+  validates :name, :presence => true, :length => { maximum: 32 }
+  validates :description, :length => { maximum: 255 }
 end
