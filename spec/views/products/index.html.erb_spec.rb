b2d14711a7063543d207fe0b4e29f05a0f0e76c7
Thinking Song <123wuming_123@163.com>
2013-12-10 14:15:55 +0800

    修改产品index view测试

--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -21,6 +21,6 @@ describe "products/index" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Sn".to_s, :count => 2
     assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Description".to_s, :count => 2
+    # assert_select "tr>td", :text => "Description".to_s, :count => 2
   end
 end
