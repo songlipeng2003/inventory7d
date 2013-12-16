10ef54b43636a02df09a258b9ed1189e654fa72e
Thinking Song <123wuming_123@163.com>
2013-12-16 14:55:12 +0800

    添加仓库名唯一性验证

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,4 +1,4 @@
 class Warehouse < ActiveRecord::Base
-    validates :name, :presence => true, :length => { maximum: 32 }
+    validates :name, :presence => true, :uniqueness => true, :length => { maximum: 32 }
     validates :description, :length => { maximum: 32 }
 end
